b9dd660faa8316a4ee019c16c8046a526babedac
Melinda Fitriani <meli@Macbooks-MacBook-Air.local>
2023-04-03 16:29:48 +0700

    get user

--- cypress.config.js
+++ cypress.config.js
@@ -2,10 +2,8 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      pageLoadTimeout: 10000000
-      // implement node event listeners here
-    },
+    pageLoadTimeout: 100000,
+    
     
   },
 
--- cypress/e2e/GetUser.cy.js
+++ cypress/e2e/GetUser.cy.js
@@ -1,13 +1,13 @@
-/// <reference types ="cypress" />
+/// <reference types="cypress" />
 
-describe('test api user gorest1', () => {
+describe('test api get user gorest', () => {
   
   let accessToken = '803b28451962a1f65264171a2385d2f37c8090951aaa7608333d1dbc2e0866c1'
   
-  it('ger users', () => {
+  it('get users', () => {
     cy.request({
       method : 'GET',
-      url : 'https://gorest.co.in/public-api/users',
+      url : 'https://gorest.co.in/public-api/users/123/posts',
       headers : {
         'authorization' : "Bearer " + accessToken
       }
@@ -15,6 +15,18 @@ describe('test api user gorest1', () => {
     .then((res) =>{
         expect(res.status).to.eq(200)
         expect(res.body.meta.pagination.limit).to.eq(10)
+        expect(res.body).has.to.deep.equal({
+              "code": 200,
+              "meta": {
+              "pagination": {
+              "total": 0,
+              "pages": 0,
+              "page": 1,
+              "limit": 10
+            }
+          },
+          "data": []
+        })
     })
   })
 
@@ -31,8 +43,5 @@ describe('test api user gorest1', () => {
         expect(res.body.data.message).to.eq("Resource not found")
     })
   })
-
-
-
 
 })
